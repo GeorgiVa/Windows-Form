551c02fb7320bda096396d19f4093c0c151cecfa
Georgi <50082312+GeorgiVa@users.noreply.github.com>
2019-05-03 16:30:07 +0300

    Add files via upload

---
+++ Currency Converter/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Currency_Converter
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if(disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+         this.button1 = new System.Windows.Forms.Button();
+         this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+         this.button2 = new System.Windows.Forms.Button();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.button3 = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // button1
+         // 
+         this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+         this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+         this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+         this.button1.Location = new System.Drawing.Point(13, 36);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(159, 30);
+         this.button1.TabIndex = 0;
+         this.button1.Text = "Open Converter";
+         this.button1.UseVisualStyleBackColor = true;
+         this.button1.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // webBrowser1
+         // 
+         this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.webBrowser1.Location = new System.Drawing.Point(-4, 72);
+         this.webBrowser1.Margin = new System.Windows.Forms.Padding(0, 3, 0, 0);
+         this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+         this.webBrowser1.Name = "webBrowser1";
+         this.webBrowser1.Size = new System.Drawing.Size(410, 325);
+         this.webBrowser1.TabIndex = 1;
+         // 
+         // button2
+         // 
+         this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+         this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+         this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+         this.button2.Location = new System.Drawing.Point(288, 36);
+         this.button2.Name = "button2";
+         this.button2.Size = new System.Drawing.Size(100, 30);
+         this.button2.TabIndex = 2;
+         this.button2.Text = "Close";
+         this.button2.UseVisualStyleBackColor = true;
+         this.button2.Click += new System.EventHandler(this.button2_Click);
+         // 
+         // textBox1
+         // 
+         this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+         this.textBox1.Location = new System.Drawing.Point(13, 7);
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(377, 23);
+         this.textBox1.TabIndex = 3;
+         // 
+         // button3
+         // 
+         this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+         this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+         this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+         this.button3.Location = new System.Drawing.Point(178, 36);
+         this.button3.Name = "button3";
+         this.button3.Size = new System.Drawing.Size(104, 30);
+         this.button3.TabIndex = 4;
+         this.button3.Text = "Go / Search ";
+         this.button3.UseVisualStyleBackColor = true;
+         this.button3.Click += new System.EventHandler(this.button3_Click);
+         // 
+         // Form1
+         // 
+         this.AcceptButton = this.button3;
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackColor = System.Drawing.Color.LightGreen;
+         this.ClientSize = new System.Drawing.Size(402, 395);
+         this.Controls.Add(this.button3);
+         this.Controls.Add(this.textBox1);
+         this.Controls.Add(this.button2);
+         this.Controls.Add(this.webBrowser1);
+         this.Controls.Add(this.button1);
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.Name = "Form1";
+         this.Text = "Currency Converter - Browser";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.WebBrowser webBrowser1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button3;
+   }
+}
+
---
+++ Currency Converter/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Converter
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+
+         string site = Settings.Default.virusa;
+         webBrowser1.Navigate(site);
+         textBox1.AppendText(site);
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         string site = "https://www.mataf.net/bg/widget/conversiontab-BGN-SEK";
+
+         ActiveForm.Width = 450;
+         ActiveForm.Height = 425;
+         webBrowser1.Navigate(site);
+         textBox1.Clear();
+         textBox1.AppendText(site);
+      }
+      private void textBox1_Enter(object sender, EventArgs e)
+      {
+         webBrowser1.Navigate(textBox1.Text);
+      }
+      private void button3_Click(object sender, EventArgs e)
+      {
+         ActiveForm.Width = 900;
+         ActiveForm.Height = 600;
+         textBox1.Clear();
+         textBox1.AppendText(Settings.Default.virusa);
+         webBrowser1.Navigate(Settings.Default.virusa);
+      }
+      private void button2_Click(object sender, EventArgs e)
+      {
+         MessageBox.Show("Good Bay :)");
+         Thread.Sleep(500);
+         base.Dispose();
+      }
+
+
+   }
+}
